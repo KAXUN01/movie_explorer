983ddaf0d14c63e65e5dac48db2fab8b6944fe77
Kasun madhushan wickramasinghe <119012876+KAXUN01@users.noreply.github.com>
2025-05-09 11:19:47 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { MovieProvider } from "./context/MovieContext";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Login from "./pages/Login";
+import MovieDetails from "./components/MovieDetails";
+import theme from "./theme";
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <MovieProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </Router>
+    </MovieProvider>
+  </ThemeProvider>
+);
+
+export default App;
---
+++ src/components/MovieDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const MovieDetails = () => {
+  const { id } = useParams();
+  return <div>Movie Details for ID: {id}</div>;
+};
+
+export default MovieDetails;
